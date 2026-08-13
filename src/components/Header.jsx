025213539626bce7efd327886c8c087bb818fdbb
Christian Schneider-Davis <schneiderdavis@aol.com>
2026-08-13 16:05:39 +0200

    Correct node and Vite files + make page dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Header() { 
   return ( 
     <header className="hero-header"> 
-      <h1>Alex Morgan</h1> 
-      <p>Visual Artist & Digital Creator</p> 
+      <h1>Christian Schneider-Davis</h1> 
+      <p>Visual Artist & Creator</p> 
       <div className="header-line"></div> 
     </header> 
   ); 
