109bbc61d8c2f4bb00b48071886150ceff35ee63
Christian Schneider-Davis <schneiderdavis@aol.com>
2026-08-13 14:17:32 +0200

    Correct File Path
    Files routed to the wrong starting folder

---
+++ src/data/artData.js
@@ -0,0 +1,83 @@
+export const artData = [ 
+  { 
+    id: 1, 
+    title: "The Fall", 
+    category: "Painting", 
+    medium: "Acrylic on Canvas", 
+    year: "2023", 
+    image: "src\assets\images\.jpg", 
+    description: "** Sold Aug 21, 2023 -$1500 ** Original Work Created And Signed By Christian Schneider-Davis. Displayed in The Local NY Gallery (13-02 44th Ave Long Island City, NYC) in August 2023. One Of One. 36 x 48. Acrylic On Canvas."  }, 
+  { 
+    id: 2, 
+    title: "...the... bird sings", 
+    category: "Painting", 
+    medium: "Acrylic & Clay on Canvas", 
+    year: "2025", 
+    image: "src\assets\images\the bird sings.HEIC", 
+    description: "** Sold June 1, 2025 - $2000 ** Original Work Created And Signed By Christian-Davis. Displayed in The Culture Lab LIC (5-25 46th Ave, Long Island City, NYC) in April 2025. One Of One. 36 x 48. Acrylic & Clay On Canvas." 
+  }, 
+  { 
+    id: 3, 
+    title: "The Infamy", 
+    category: "Painting", 
+    medium: "Acrylic on Raw Canvas", 
+    year: "2023", 
+    image: "src\assets\images\The Infamy Christian Pharaoh Schneider-Davis.jpg", 
+    description: "Original Work Created And Signed By Christian Schneider-Davis. Displayed in The Local NY Gallery (13-02 44th Ave, Long Island City, NYC) in August 2023. One Of One. 36 x 48. Acrylic On Raw Canvas." 
+  }, 
+  { 
+    id: 4, 
+    title: "The Rose", 
+    category: "Painting", 
+    medium: "Acrylic on Canvas", 
+    year: "2023", 
+    image: "src\assets\images\The Rose Christian Pharaoh Schneider-Davis.jpg", 
+    description: "** Sold Aug 22, 2023 - $2000 ** Original Work Created And Signed By Christian Schneider-Davis. Displayed in The Local NY Gallery (13-02 44th Ave, Long Island City, NYC) in August 2023. One Of One. 36 x 48. Acrylic On Raw Canvas." 
+  },
+    { 
+    id: 5, 
+    title: "The Light", 
+    category: "Painting", 
+    medium: "Acrylic on Canvas", 
+    year: "2023", 
+    image: "src\assets\images\The Light Christian Pharaoh Schneider-Davis.jpg", 
+    description: "** Sold May 29, 2025 - $800 ** Original Work Created And Signed By Christian Schneider-Davis. Displayed in The Local NY Gallery (13-02 44th Ave, Long Island City, NYC) in August 2023. Displayed in The Culture Lab LIC (5-25 46th Ave, Long Island City, NYC) in April 2025. One Of One. 36 x 48. Acrylic On Canvas." 
+  }, 
+  { 
+    id: 6, 
+    title: "Skin Deep", 
+    category: "Painting", 
+    medium: "Acrylic on Canvas", 
+    year: "", 
+    image: "", 
+    description: "" 
+  }, 
+  { 
+    id: 7, 
+    title: "", 
+    category: "Painting", 
+    medium: "Acrylic on Canvas", 
+    year: "", 
+    image: "", 
+    description: "" 
+  }, 
+  { 
+    id: 8, 
+    title: "", 
+    category: "Painting", 
+    medium: "Acrylic on Canvas", 
+    year: "", 
+    image: "", 
+    description: "" 
+  }, 
+  { 
+    id: 9, 
+    title: "", 
+    category: "Painting", 
+    medium: "Acrylic on Canvas", 
+    year: "", 
+    image: "", 
+    description: "" 
+  } 
+
+];
