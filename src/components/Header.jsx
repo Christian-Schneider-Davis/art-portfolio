f457f24f2f49e154f128b5b1bdcb1ab17ae3f3ab
Christian Schneider-Davis <schneiderdavis@aol.com>
2026-08-13 21:36:37 +0200

    Add a second photo per ArtCard
    When the art.image[0] is clicked on the home page, art.image[0] and art.image[1] are displayed.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() { 
   return ( 
     <header className="hero-header"> 
-      <h1>Christian Schneider-Davis</h1> 
+      <h1>Christian Schneider-Davis</h1>
       <p>Visual Artist & Creator</p> 
       <div className="header-line"></div> 
     </header> 
