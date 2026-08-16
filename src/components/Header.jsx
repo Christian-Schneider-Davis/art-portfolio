107693a606eba443588efba5d15d81e39733da96
Christian Schneider-Davis <schneiderdavis@aol.com>
2026-08-16 23:18:16 +0200

    Roll back changes
    "Update header"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ export default function Header() {
   return ( 
     <header className="hero-header"> 
       <h1>Christian Schneider-Davis</h1>
+      <p>Visual Artist</p>
       <div className="header-line"></div> 
     </header> 
   ); 
